fd1828c063cf246730002e805b39ccb8069a3784
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-25 06:04:52 +0100

    test ctypeSchema successful!

---
+++ api/attester/ctypeSchema.js
@@ -0,0 +1,19 @@
+import * as Kilt from "@kiltprotocol/sdk-js"
+
+export function getCtypeSchema() {
+    return Kilt.CType.fromSchema({
+        $schema: 'http://kilt-protocol.org/draft-01/ctype#',
+        title: 'Event Tickets',
+        properties: {
+        name: {
+            type: 'string'
+        },
+        age: {
+            type: 'integer'
+        }
+        },
+        type: 'object'
+    })
+}
+
+console.log(getCtypeSchema())
