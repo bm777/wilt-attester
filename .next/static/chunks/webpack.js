94a7acdc8de7dec712ceff45e01f49daba3b0dcf
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-26 18:01:05 +0100

    Added IconMessage for request and verification notifications

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c23a839031a38a07"; }
+/******/ 		__webpack_require__.h = function() { return "2b3de37c64e19043"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import QRCode from "react-qr-code";
 import { useState, useEffect } from "react";
 import env from "./api/attester/env.json" assert {type: "json"}
-
-
+import iconMessage from "../public/email.png"
+import Image from 'next/image'
 
 export default function Home() {
     
@@ -31,10 +31,18 @@ export default function Home() {
     return <div className="bg-[#1E293B] w-3/5 h-auto
                 rounded-xl mx-auto my-20 drop-shadow-xl 
                 border border-slate-700">
+                
                 <div className="">
+                    <div>
+                      <div className="flex justify-end mr-10">
+                        <Image src={iconMessage} />
+                        <div className="w-7 h-7 bg-indigo-600 rounded-full flex justify-center text-white font-bold text-lg">{5}</div>
+                      </div>
+
+                    </div>
                     <div className="flex justify-center">
-                        <p className="text-[#C2E4F0] text-5xl mt-6">Wilt Authority -</p> 
-                        <p className="text-teal-600 text-5xl mt-6 ml-2 font-normal">{isAttester? "Attester": "Verifier"}</p></div>
+                        <p className="text-[#C2E4F0] text-5xl">Wilt Authority -</p> 
+                        <p className="text-teal-600 text-5xl ml-2 font-normal">{isAttester? "Attester": "Verifier"}</p></div>
                     <div className="flex justify-center"><p className="text-slate-500 text-xl font-light mt-10">Check-in verication system based on the Kilt protocol.</p></div>
                     <div className="flex justify-center gap-5 mt-10">
                         <button className="bg-indigo-600 text-gray-300 font-bold py-2 px-4 rounded inline-flex items-center" onClick={handleClick}>
