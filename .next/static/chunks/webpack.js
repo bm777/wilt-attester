85bb5a7d0a4fdec198bdc42d4f5b25aae8020fa6
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-26 05:02:45 +0100

    QR code generation successful

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99e67beb6c3579ca"; }
+/******/ 		__webpack_require__.h = function() { return "0cd8989b377b6a2c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/did/[id].js
+++ pages/did/[id].js
@@ -1,6 +1,21 @@
 import { useRouter } from "next/router";
+import QRCode from "react-qr-code";
+import { useState } from "react";
 
 export default function Content() {
+    // router to get the did value
     const router = useRouter()
-    return <div>Did: {router.query.id}</div>
+    const did = router.query.id
+
+    // the state
+    const  [qr, setQr] = useState(did || "")
+    
+
+    return <div>
+                <div>Did: {did}</div>
+                {did != "" && (
+                    <QRCode value={qr} className="" />
+                )}
+        
+            </div>
 }
