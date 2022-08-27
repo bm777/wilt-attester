140ca4b25d354803508601bcddf9d4987dac4c67
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-27 15:36:01 +0100

    styled the table of requests

--- components/Tx.js
+++ components/Tx.js
@@ -1,16 +1,29 @@
 
 
 export default function Tx(props) {
+    const isData = props.data.type
 
-
+    if (isData == "TYPE") {
+        return (
+            <div className="mx-auto mt-3 h-10 grid grid-cols-10 items-center justify-start  gap-5 border border-indigo-700 bg-indigo-600 w-9/12 rounded">
+                <div className="ml-3 w-3 h-3 rounded-full  my-auto"></div>
+                <p className="col-span-2 text-white">{props.data.type}</p>
+                <p className="col-span-3 text-white">{props.data._id}</p>
+                <p className="text-white">{props.data.name}</p>
+                <p className="text-white">{props.data.age}</p>
+                <p className="col-span-2 rounded text-white">Actions</p>
+            </div>
+        )
+        
+    }
+    
     return (
-
         <div className="mx-auto mt-3 h-10 grid grid-cols-10 items-center justify-start  gap-5 border border-indigo-700 w-9/12 rounded">
             <div className="ml-3 w-3 h-3 rounded-full bg-teal-600 my-auto"></div>
-            <p className="col-span-2 text-white">{props.data.type}</p>
-            <p className="col-span-3 text-amber-400">{props.data._id}</p>
-            <p className="text-slate-400">{props.data.name}</p>
-            <p className="text-slate-400">{props.data.age}</p>
+            <p className="col-span-2 text-white font-light">{props.data.type}</p>
+            <p className="col-span-3 text-amber-400 font-light">{props.data._id}</p>
+            <p className="text-slate-400 font-light">{props.data.name}</p>
+            <p className="text-slate-400 font-light">{props.data.age}</p>
             <button className="bg-indigo-700 rounded text-slate-300">approve</button>
             <button className="bg-red-500 rounded text-slate-300">deny</button>
         </div>
