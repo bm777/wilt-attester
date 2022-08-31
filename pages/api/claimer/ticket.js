b65edac35f2dd24c740229841162f395cb249200
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-31 09:39:01 +0100

    update

--- pages/api/claimer/ticket.js
+++ pages/api/claimer/ticket.js
@@ -1,10 +1,19 @@
 import { generateRequest } from "./generateRequest.js"
 
-
 export default async function handler(req, res) {
   try {
-    console.log(req.body)
-    res.status(200).json({ response: JSON.stringify(req.body)})
+    var _body = req.body
+    var data = JSON.parse(JSON.parse(_body))
+    const content = {name: data.name, age: data.age}
+    
+    const mnemonic = data.mnemonic
+    
+    const request = await generateRequest(content, mnemonic)
+    console.log(request)
+
+
+
+    res.status(200).json({ response: JSON.stringify(request)})
   } catch (error) {
     console.log(error)
     res.status(200).json({ response: error})
